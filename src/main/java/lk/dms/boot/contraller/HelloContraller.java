3ead2f04d75caf9fa35cc2940e25f3c5bf49b33a
Siridesha Nadeeshanka <mailtosiridesha@gmail.com>
2021-09-12 19:59:02 +0530

    create AppInilizer

---
+++ src/main/java/lk/dms/boot/contraller/HelloContraller.java
@@ -0,0 +1,14 @@
+package lk.dms.boot.contraller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloContraller {
+	
+	@GetMapping("/")
+	public String getvalue() {
+		return "Hellow world!";
+	}
+
+}
